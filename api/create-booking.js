559bdeb728a50e85ad87e328f9888a9e3ef08d28
gradysteer-beep <gradysteer@gmail.com>
2026-03-09 15:09:47 +0000

    create-booking.js

--- api/create-booking.js
+++ api/create-booking.js
@@ -1,5 +1,15 @@
-export default async function handler(request, response) {
-  response.status(200).json({
-    status: "API working"
-  });
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" })
+  }
+
+  const { name, guests, date, time, phone } = req.body
+
+  console.log("Booking received:", name, guests, date, time, phone)
+
+  res.status(200).json({
+    success: true,
+    message: "Booking received"
+  })
 }
